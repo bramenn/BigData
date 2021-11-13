8b29f20cd0a223199e464bc6938a932ff63c79e1
Alejandro Herrera <brayan.herrera@utp.edu.co>
2021-11-13 08:55:33 -0500

    Example is added whit luigi parameters

--- luigi/test_luigi.py
+++ luigi/test_luigi.py
@@ -1,17 +1,18 @@
 import luigi
 
-# --local-scheduler
+# python3 -m luigi --module test_luigi Cuadrados --n 15 --local-scheduler
 class SalidaNumeros(luigi.Task):
     """Creamos un archivo y le asiganos numeros del 1 al 11"""
+    n = luigi.IntParameter()
     def requires(self):
         return []
 
     def output(self):
-        return luigi.LocalTarget("numeros.txt")
+        return luigi.LocalTarget(f"numeros_{self.n}.txt")
 
     def run(self):
         with self.output().open("w") as f:
-            for i in range(1,11):
+            for i in range(1,self.n+1):
                 f.write(f"{i}\n")
 
 class Cuadrados(luigi.Task):
@@ -19,11 +20,14 @@ class Cuadrados(luigi.Task):
     Toma el proceso anterior y saca los numeros y los eleva al cuadrado
     Los resultados los guarda en un archivo diferente
     """
+
+    n = luigi.IntParameter(default=10)
+
     def requires(self):
-        return [SalidaNumeros()]
+        return [SalidaNumeros(n = self.n)]
 
     def output(self):
-        return luigi.LocalTarget("cuadrados.txt")
+        return luigi.LocalTarget(f"cuadrados_{self.n}.txt")
 
     def run(self):
         with self.input()[0].open() as fin, self.output().open("w") as fout:
