f3b56a09e97a6ca09a90432d9a9fda8e20099fb4
Alejandro Herrera <brayan.herrera@utp.edu.co>
2021-11-20 10:52:52 -0500

    Added Taller Drogas

--- Drogas/modelo.py
+++ Drogas/modelo.py
@@ -1,14 +1,23 @@
 import db
 from sqlalchemy import Column, Integer, Text, Boolean, String, ForeignKey, Float
+from sqlalchemy.orm import relationship
 
+class Departamento(db.Base):
+    __tablename__ = "departamento"
+    id = Column(Integer, primary_key=True)
+    departamento = Column("departamento", Text)
+    municipio = relationship("Municipio")
+class Municipio(db.Base):
+    __tablename__ = "municipio"
+    codigodane = Column("codigodane", Text, primary_key=True)
+    municipio = Column("municipio", Text)
+    id_dept = Column(Integer, ForeignKey("departamento.id"))
+    incautacion = relationship("Incautacion")
 
 class Incautacion(db.Base):
     __tablename__ = "incautacion"
     id = Column(Integer, primary_key=True)
-    departamento = Column("departamento", Text)
-    municipio = Column("municipio", Text)
-    codigodane = Column("codigodane", Text)
     clasebien = Column("clasebien", Text)
     fecha = Column("fecha", Text)
     cantidad = Column("cantidad", Float)
-
+    id_muni = Column(Text, ForeignKey("municipio.codigodane"))
--- Drogas/sondeo.py
+++ Drogas/sondeo.py
@@ -4,28 +4,52 @@ import db
 from modelo import *
 
 # python3 sondeo.py inacutacion.csv 10
+DEPARTAMENTOS = []
+MUNICIPIOS = []
 
 
-def insert_inca(departamento, municipio, codigodane, clasebien, fecha, cantidad):
-    """Inserta una fila en incautacion dado los argumentos"""
+def insert_data(data):
+    """Inserta una fila"""
+    db.session.add(data)
+    db.session.commit()
+
+def serach_departamento_id(departamento):
+    dept = db.session.query(Departamento).where(Departamento.departamento==departamento).first()
+    return dept.id
+
+def serach_municipio_id(municipio):
+    muni = db.session.query(Municipio).where(Municipio.municipio==municipio).first()
+    return muni.codigodane
+
+def incautacion_model(codigodane, clasebien, fecha, cantidad):
     inca = Incautacion(
-        departamento=departamento,
-        municipio=municipio,
-        codigodane=codigodane,
         clasebien=clasebien,
         fecha=fecha,
         cantidad=cantidad,
+        id_muni=codigodane
     )
-    db.session.add(inca)
-    db.session.commit()
+    return inca
 
+def departamento_model(departamento):
+    dept = Departamento(
+        departamento=departamento
+    )
+    return dept
+
+def municipio_model(codigodane, municipio, id_dept):
+    muni = Municipio(
+        codigodane=codigodane,
+        municipio=municipio,
+        id_dept=id_dept
+    )
+    return muni
 
 if __name__ == "__main__":
+
     db.Base.metadata.create_all(db.conn)
     arg = sys.argv
 
     if len(arg) == 3:
-        print("Cantidad de argumentos correctos!")
         file_name = arg[1]
         element_len = arg[2]
 
@@ -40,15 +64,37 @@ if __name__ == "__main__":
             if ignorar:
                 ignorar = False
             else:
-                # Insertamos la informacion
-                insert_inca(
-                    fields[0],
-                    fields[1],
-                    fields[2],
-                    fields[3],
-                    fields[4],
-                    float(fields[5]),
-                )
+                # Obrenemos la infromacion
+                departamento = fields[0]
+                municipio = fields[1]
+                codigodane = fields[2]
+                clasebien = fields[3]
+                fecha = fields[4]
+                cantidad = float(fields[5])
+
+                if not departamento in DEPARTAMENTOS:
+                    DEPARTAMENTOS.append(departamento)
+                    dept_model = departamento_model(departamento)
+                    insert_data(dept_model)
+
+
+                if not municipio in MUNICIPIOS:
+                    MUNICIPIOS.append(municipio)
+                    id_dept = serach_departamento_id(departamento)
+                    if id_dept:
+                        muni_model = municipio_model(codigodane, municipio, id_dept)
+                        insert_data(muni_model)
+                    else:
+                        print(f"No se inserto el registro municipio, no se encontro {departamento}")
+
+
+                id_muni = serach_municipio_id(municipio)
+                if id_muni:
+                    inca_model = incautacion_model(id_muni, clasebien, fecha, cantidad)
+                    insert_data(inca_model)
+                else:
+                    print(f"No se inserto el registro incautacion, no se encontro {municipio}")
+
             if counter >= limit_data:
                 break
 
@@ -56,3 +102,5 @@ if __name__ == "__main__":
 
     else:
         print("### Argumentos incorrectos")
+
+    print("\nAll ready!! (∩｀-´)⊃━☆ﾟ.*･｡ﾟ")
